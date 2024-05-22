2294d9062ceea1b069276f073c815d8ba5afd72e
shenghong0914 <154745647+shenghong0914@users.noreply.github.com>
2024-05-22 20:33:59 +0800

    變更文字

--- Rename.py
+++ Rename.py
@@ -14,4 +14,9 @@ for filename in os.listdir(folder_path):
     os.rename(f"{folder_path}\\{filename}", f"{folder_path}\\{new_filename}")
 
 # 批次處理完成
-print("批次處理已完成。")
+print("已完成。")
+
+print ("hihi")
+
+
+
